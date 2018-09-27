451568f44f221d7d4e13761539003ef4d6bea3dd
Artyom Y. Razinov <arazinov@avito.ru>
2018-09-27 16:24:01 +0300

    MBS-3552: Support tags in runtime dump

--- Sources/RuntimeDump/RuntimeTestEntry.swift
+++ Sources/RuntimeDump/RuntimeTestEntry.swift
@@ -5,6 +5,37 @@ public struct RuntimeTestEntry: Codable, CustomStringConvertible {
     public let path: String
     public let testMethods: [String]
     public let caseId: UInt?
+    public let tags: [String]
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        className = try container.decode(String.self, forKey: .className)
+        path = try container.decode(String.self, forKey: .path)
+        testMethods = try container.decode([String].self, forKey: .testMethods)
+        caseId = try container.decodeIfPresent(UInt.self, forKey: .caseId)
+        tags = try container.decodeIfPresent([String].self, forKey: .tags) ?? []
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(className, forKey: .className)
+        try container.encode(path, forKey: .path)
+        try container.encode(testMethods, forKey: .testMethods)
+        if let caseId = caseId {
+            try container.encode(caseId, forKey: .caseId)
+        }
+        if !tags.isEmpty {
+            try container.encode(tags, forKey: .tags)
+        }
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case className
+        case path
+        case testMethods
+        case caseId
+        case tags
+    }
     
     public var description: String {
         var identifyingComponents = [String]()
@@ -16,6 +47,18 @@ public struct RuntimeTestEntry: Codable, CustomStringConvertible {
         let testMethodsJoined = testMethods.joined(separator: "|")
         let identifyingComponentsJoined = identifyingComponents.joined(separator: ",")
         
-        return "(\(RuntimeTestEntry.self): \(identifyingComponentsJoined) / [\(testMethodsJoined)])"
+        var topLevelComponents = [
+            identifyingComponentsJoined,
+            "[\(testMethodsJoined)]"
+        ]
+        
+        if !tags.isEmpty {
+            let tagsJoined = tags.joined(separator: "|")
+            topLevelComponents.append("tags [\(tagsJoined)]")
+        }
+        
+        let topLevelComponentsJoined = topLevelComponents.joined(separator: " / ")
+        
+        return "(\(RuntimeTestEntry.self): \(topLevelComponentsJoined)"
     }
 }
