61be17d0dfbdbf3518b42c251a38104e79e99d44
Vladislav Alekseev <vvalekseev@avito.ru>
2018-10-25 11:01:23 +0300

    MBS-3416: add not equal test

--- Tests/ModelsTests/ModelsTests.swift
+++ Tests/ModelsTests/ModelsTests.swift
@@ -20,4 +20,40 @@ final class ModelsTests: XCTestCase {
         
         XCTAssertEqual(bucket1.bucketId, bucket2.bucketId)
     }
+    
+    func testBucketsHaveDifferentIdsForDifferentTestEntries() throws {
+        let bucket1 = Bucket(
+            testEntries: [
+                TestEntry(className: "-----", methodName: "testMethod", caseId: nil),
+                TestEntry(className: "class", methodName: "testAnotherMethod", caseId: nil)
+            ],
+            testDestination: try TestDestination(deviceType: "device", iOSVersion: "11.3"))
+        
+        let bucket2 = Bucket(
+            testEntries: [
+                TestEntry(className: "class", methodName: "testAnotherMethod", caseId: nil),
+                TestEntry(className: "class", methodName: "testMethod", caseId: nil)
+            ],
+            testDestination: try TestDestination(deviceType: "device", iOSVersion: "11.3"))
+        
+        XCTAssertNotEqual(bucket1.bucketId, bucket2.bucketId)
+    }
+    
+    func testBucketsHaveDifferentIdsForDifferentTestDestinations() throws {
+        let bucket1 = Bucket(
+            testEntries: [
+                TestEntry(className: "class", methodName: "testMethod", caseId: nil),
+                TestEntry(className: "class", methodName: "testAnotherMethod", caseId: nil)
+            ],
+            testDestination: try TestDestination(deviceType: "device", iOSVersion: "11.3"))
+        
+        let bucket2 = Bucket(
+            testEntries: [
+                TestEntry(className: "class", methodName: "testMethod", caseId: nil),
+                TestEntry(className: "class", methodName: "testAnotherMethod", caseId: nil)
+            ],
+            testDestination: try TestDestination(deviceType: "device", iOSVersion: "11.4"))
+        
+        XCTAssertNotEqual(bucket1.bucketId, bucket2.bucketId)
+    }
 }
