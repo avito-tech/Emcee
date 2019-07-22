05d85f4520f789add277924b4c9c761007fa6db4
Vladislav Alekseev <vvalekseev@avito.ru>
2019-07-22 17:00:54 +0300

    Replace Makefile with script

--- Makefile
+++ Makefile
@@ -1,34 +1,17 @@
-install_deps:
-	brew ls --versions pkg-config > /dev/null || brew install pkg-config
-	brew ls --versions libssh2 > /dev/null || brew install libssh2
-
-generate: install_deps
-	swift package generate-xcodeproj --xcconfig-overrides Package.xcconfig --enable-code-coverage
-.PHONY: generate
-
-open: generate
-	open *.xcodeproj
-.PHONY: open
+open: 
+	bash make.sh open
 
 clean:
-	rm -rf .build/
-	rm -rf TestPlugin/.build/
-.PHONY: clean
+	bash make.sh clean
 
-build: install_deps
-	swift build -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.13" --static-swift-stdlib
+build: 
+	bash make.sh build
 
-run: install_deps
-	swift run -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.13" --static-swift-stdlib
+test:
+	bash make.sh test
 
-test: install_deps
-	swift test -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.13" 
+test-parallel:
+	bash make.sh test-parallel
 
-test-parallel: install_deps
-	swift test -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.13" --parallel
-
-integration-test: install_deps
-	auxiliary/run_smoke_tests.sh
-
-rerun-integration-test: install_deps
-	PYCACHE_ENABLED=true auxiliary/run_smoke_tests.sh
+integration-test:
+	bash make.sh integration-test
---
+++ make.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+set -x
+
+function expose_required_developer_dir() {
+	required_xcode_version=$1
+
+	savedIFS=$IFS
+	IFS=$'\n'
+	for xcode_path in $(mdfind -onlyin /Applications "kMDItemCFBundleIdentifier = com.apple.dt.Xcode")
+	do
+		xcode_version=$(defaults read "$xcode_path/Contents/Info.plist" CFBundleShortVersionString)
+		if [[ $xcode_version == $required_xcode_version ]]
+		then
+			DEVELOPER_DIR="$xcode_path/Contents/Developer"
+			echo "Found Xcode with version $required_xcode_version at: $DEVELOPER_DIR"
+			return 0
+		fi
+	done
+	IFS=$savedIFS
+	
+	echo "ERROR: can't locate Xcode with version: '$required_xcode_version'"
+	exit 1
+}
+
+function install_deps() {
+	brew ls --versions pkg-config > /dev/null || brew install pkg-config
+	brew ls --versions libssh2 > /dev/null || brew install libssh2
+	expose_required_developer_dir "10.1"
+}
+
+function open_xcodeproj() {
+	generate_xcodeproj
+	open *.xcodeproj
+}
+
+function generate_xcodeproj() {
+	install_deps
+	DEVELOPER_DIR="$DEVELOPER_DIR" swift package generate-xcodeproj --xcconfig-overrides Package.xcconfig --enable-code-coverage
+}
+
+function clean() {
+	rm -rf .build/
+	rm -rf TestPlugin/.build/
+}
+
+function build() {
+	install_deps
+	DEVELOPER_DIR="$DEVELOPER_DIR" swift build -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.13"
+}
+
+function run_tests() {
+	install_deps
+	DEVELOPER_DIR="$DEVELOPER_DIR" swift test -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.13" 
+}
+
+function run_tests_parallel() {
+	install_deps
+	DEVELOPER_DIR="$DEVELOPER_DIR" swift test -Xswiftc "-target" -Xswiftc "x86_64-apple-macosx10.13" --parallel
+}
+
+function run_integration_tests() {
+	install_deps
+	DEVELOPER_DIR="$DEVELOPER_DIR" auxiliary/run_smoke_tests.sh
+}
+
+case "$1" in
+    generate)
+        generate_xcodeproj
+        ;;
+    open)
+    	open_xcodeproj
+    	;;
+    test)
+        run_tests
+        ;;
+    integration-test)
+        run_integration_tests
+        ;;
+    test-parallel)
+    	run_tests_parallel
+    	;;
+    build)
+    	build
+    	;;
+    clean)
+    	clean
+    	;;
+esac
