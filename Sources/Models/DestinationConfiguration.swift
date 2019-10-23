83012ce58ade7dff85b2670621ab825bb25299ee
Vladislav Alekseev <vvalekseev@avito.ru>
2019-10-23 14:51:27 +0300

    Remove legacy code

--- Sources/Models/DestinationConfiguration.swift
+++ Sources/Models/DestinationConfiguration.swift
@@ -1,13 +1,10 @@
 import Foundation
 
 public struct DestinationConfiguration: Decodable {
-    /** This is an identifier of the DeploymentDestination */
+    /// This is an identifier of the DeploymentDestination
     public let destinationIdentifier: WorkerId
     
-    /**
-     * The value for the number of simulators that can be used on this destination.
-     * This overrides the value provided in LocalTestRunConfiguration.
-     */
+    /// The value for the number of simulators that can be used on this destination.
     public let numberOfSimulators: UInt
 
     public init(destinationIdentifier: WorkerId, numberOfSimulators: UInt) {
