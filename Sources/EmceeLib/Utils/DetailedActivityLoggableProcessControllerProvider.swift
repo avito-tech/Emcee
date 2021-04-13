c28ac1ccc1c20c4d1f50e3cdd060c4582edc0593
Vladislav Alekseev <vvalekseev@avito.ru>
2021-04-13 16:14:14 +0300

    Print log output location when process starts

--- Sources/EmceeLib/Utils/DetailedActivityLoggableProcessControllerProvider.swift
+++ Sources/EmceeLib/Utils/DetailedActivityLoggableProcessControllerProvider.swift
@@ -1,44 +1,39 @@
 import DI
 import Foundation
 import EmceeLogging
+import Extensions
 import LoggingSetup
 import PathLib
 import ProcessController
 
 public final class DetailedActivityLoggableProcessControllerProvider: ProcessControllerProvider {
-    private let processControllerProvider: LoggableProcessControllerProvider
-    
     private let di: DI
     
     public init(
         di: DI
     ) throws {
         self.di = di
+    }
+    
+    public func createProcessController(subprocess: Subprocess) throws -> ProcessController {
+        let logger = try di.get(ContextualLogger.self)
         
-        processControllerProvider = LoggableProcessControllerProvider(
-            pathProvider: { [di] processName -> (stdout: AbsolutePath, stderr: AbsolutePath) in
-                let logger = try di.get(ContextualLogger.self)
-                
-                let paths = try di.get(LoggingSetup.self).childProcessLogsContainerProvider().paths(subprocessName: processName)
-                logger.debug(
-                    "Subprocess output will be stored: stdout: \(paths.stdout) stderr: \(paths.stderr)",
-                    subprocessPidInfo: PidInfo(pid: 0, name: processName)
-                )
-                return paths
-            },
+        let paths = try di.get(LoggingSetup.self).childProcessLogsContainerProvider().paths(
+            subprocessName: try subprocess.processName()
+        )
+        
+        let processControllerProvider = LoggableProcessControllerProvider(
+            pathProvider: { _ in paths },
             provider: DefaultProcessControllerProvider(
                 dateProvider: try di.get(),
                 fileSystem: try di.get()
             )
         )
-    }
-    
-    public func createProcessController(subprocess: Subprocess) throws -> ProcessController {
-        let logger = try di.get(ContextualLogger.self)
+
         let processController = try processControllerProvider.createProcessController(subprocess: subprocess)
         
         processController.onStart { [logger] sender, _ in
-            logger.debug("Started subprocess: \(sender.subprocess)", subprocessPidInfo: sender.subprocessInfo.pidInfo)
+            logger.debug("Started subprocess: \(sender.subprocess), output will be stored: stdout: \(paths.stdout) stderr: \(paths.stderr)", subprocessPidInfo: sender.subprocessInfo.pidInfo)
         }
         
         processController.onSignal { [logger] sender, signal, _ in
@@ -50,5 +45,11 @@ public final class DetailedActivityLoggableProcessControllerProvider: ProcessCon
         }
         
         return processController
+    }
+}
+
+private extension Subprocess {
+    func processName() throws -> String {
+        return try arguments[0].stringValue().lastPathComponent
     }
 }
